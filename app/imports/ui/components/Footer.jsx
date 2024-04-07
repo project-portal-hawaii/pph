920fd95256af4108dbed4f128188b7ae4615d173
DMITRY GORDEEV <gordeev.du@gmail.com>
2024-04-06 19:44:06 -1000

    Logo and footer update
    Changed logo and footer from bowfolio template to our project name

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => (
   <footer className="footer mt-auto py-3 bg-dark">
     <Container>
       <Col className="text-center" style={{ color: 'white' }}>
-        The Bowfolios Project
+        Project Portal Hawaii
         {' '}
         <br />
         University of Hawaii
@@ -14,7 +14,7 @@ const Footer = () => (
         Honolulu, HI 96822
         {' '}
         <br />
-        <a style={{ color: 'white' }} href="https://bowfolios.github.io">https://bowfolios.github.io</a>
+        <a style={{ color: 'white' }} href="https://project-portal-hawaii.github.io">https://project-portal-hawaii.github.io</a>
       </Col>
     </Container>
   </footer>

7ba77ef65bccc969bdc82a0fca23345683857a75
Adam Bell <belladam@hawaii.edu>
2024-05-02 13:13:40 -1000

    Resolved Lint Errors

--- app/imports/ui/pages/SignUp.jsx
+++ app/imports/ui/pages/SignUp.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link, Navigate } from 'react-router-dom';
 import { Accounts } from 'meteor/accounts-base';
-import { Alert, Card, Col, Container, Row } from 'react-bootstrap';
+import { Alert, Card, Col, Row } from 'react-bootstrap';
 import SimpleSchema from 'simpl-schema';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import { AutoForm, ErrorsField, SubmitField, TextField } from 'uniforms-bootstrap5';

380e5da80e79cdf8ef9cfcf5b7232d0fb9ecd281
GabrielWhite808 <156386680+GabrielWhite808@users.noreply.github.com>
2024-05-07 17:43:52 -1000

    Showcase Projects dropdown

--- app/imports/ui/pages/ShowcaseProjects.jsx
+++ app/imports/ui/pages/ShowcaseProjects.jsx
@@ -56,15 +56,9 @@ const MakeCard = ({ project }) => (
           open={false}
           title="Show all information"
         >
-          Consectetur adipiscing elit pellentesque habitant morbi tristique.
-          Pulvinar pellentesque habitant morbi tristique. Vel quam elementum
-          pulvinar etiam. Pulvinar pellentesque habitant morbi tristique senectus
-          et netus et. Elementum integer enim neque volutpat. Faucibus in ornare
-          quam viverra orci sagittis. Amet volutpat consequat mauris nunc congue
-          nisi vitae suscipit. Dui accumsan sit amet nulla. Proin sagittis nisl
-          rhoncus mattis. Enim nulla aliquet porttitor lacus. Arcu odio ut sem
-          nulla pharetra diam sit amet. Gravida rutrum quisque non tellus orci ac
-          auctor augue
+          <Image
+            source={project.image}
+          />
         </Collapsible>
       </Card.Body>
     </Card>
